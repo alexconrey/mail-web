af4b1797af8d09a4b80103089d7c0661be7960d4
Alex Conrey <alex@conrey.us>
2018-01-14 18:49:57 -0600

    added domain tests

--- domains/tests.py
+++ domains/tests.py
@@ -3,4 +3,33 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+
+from domains.models import Domain, ForwardDomain
+
 # Create your tests here.
+
+class DomainTest(TestCase):
+    def setUp(selt):
+            User.objects.create_user('test', 'test@user.com', 'test123')
+            Domain.objects.create(name='test.com',owner_id='1')
+            ForwardDomain.objects.create(name='forwardtest.com', owner_id='1', to_domain_id='1')
+
+    def test_domain_exists(self):
+        domain_lookup = Domain.objects.filter(name='test.com').count()
+        self.assertEqual(domain_lookup,1)
+
+    def test_forward_domain_exists(self):
+        fwd_domain_lookup = ForwardDomain.objects.filter(name='forwardtest.com').count()
+        self.assertEqual(fwd_domain_lookup, 1)
+
+    def test_domain_delete(self):
+        Domain.objects.get(name='test.com').delete()
+        domain_lookup = Domain.objects.filter(name='test.com').count()
+        self.assertEqual(domain_lookup,0)
+        
+    def test_forward_domain_delete(self):
+        ForwardDomain.objects.get(name='forwardtest.com').delete()
+        fwd_domain_lookup = ForwardDomain.objects.filter(name='forwardtest.com').count()
+        self.assertEqual(fwd_domain_lookup,0)
+

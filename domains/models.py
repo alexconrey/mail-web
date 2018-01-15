d65d1947b56bdd8fa9e2077c2eabcc32675c88f7
Alex Conrey <alex@conrey.us>
2018-01-14 20:30:51 -0600

    added model updates

--- domains/models.py
+++ domains/models.py
@@ -4,24 +4,29 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+from datetime import datetime
 
 # Create your models here.
 
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return datetime.now()
+
 class Domain(models.Model):
 	name = models.CharField(max_length=255)
-	owner = models.ForeignKey(User, default=1)
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_modified = models.DateTimeField(auto_now=True)
+	owner = models.ForeignKey(User, default=1, related_name='domains_domain')
+	date_created = models.DateTimeField(default=datetime.now())
+        date_modified = AutoDateTimeField(default=datetime.now())
 	
 	def __unicode__(self):
 		return self.name
 
 class ForwardDomain(models.Model):
 	name = models.CharField(max_length=255)
-	owner = models.ForeignKey(User, default=1)
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_modified = models.DateTimeField(auto_now=True)
-	to_domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
+	owner = models.ForeignKey(User, default=1, related_name='domains_forwardeddomain')
+	date_created = models.DateTimeField(default=datetime.now())
+        date_modified = AutoDateTimeField(default=datetime.now())
+	to_domain = models.ForeignKey(Domain, on_delete=models.CASCADE, related_name='domains_forwardeddomain')
 
 	def __unicode__(self):
 		return self.name
--- mailboxes/models.py
+++ mailboxes/models.py
@@ -5,35 +5,42 @@ from django.db import models
 
 from datetime import datetime
 
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return datetime.now()
+
+
 # Create your models here.
 class Mailbox(models.Model):
         name = models.CharField(max_length=32)
         password = models.CharField(max_length=256)
-        domain = models.ForeignKey('domains.Domain', on_delete=models.CASCADE)
+        domain = models.ForeignKey('domains.Domain', on_delete=models.CASCADE, related_name='mailboxes_mailbox')
         quota = models.IntegerField(default=0)
         is_active = models.BooleanField(default=True)
         is_readonly = models.BooleanField(default=False)
-	date_created = models.DateTimeField(auto_now_add=True, blank=True)
-	date_modified = models.DateTimeField(auto_now=True, blank=True)
+      	date_created = models.DateTimeField(default=datetime.now())
+        date_modified = AutoDateTimeField(default=datetime.now())
 
-        def __unicode__(self):
-                return self.name + '@' + self.domain.name
 	class Meta:
 		verbose_name = 'Mailbox'
 		verbose_name_plural = 'Mailboxes'
 
+        def __unicode__(self):
+                return self.name + '@' + self.domain.name
+
 
 class Alias(models.Model):
         name = models.CharField(max_length=255)
-        domain = models.ForeignKey('domains.Domain', on_delete=models.CASCADE)
+        domain = models.ForeignKey('domains.Domain', on_delete=models.CASCADE, related_name='mailboxes_alias')
         to_mailbox = models.CharField(max_length=255)
-	date_created = models.DateTimeField(auto_now_add=True)
-	date_modified = models.DateTimeField(auto_now=True)
+       	date_created = models.DateTimeField(default=datetime.now())
+        date_modified = AutoDateTimeField(default=datetime.now())
 
-        def __unicode__(self):
-                return self.name + '@' + self.domain.name
 	
 	class Meta:
 		verbose_name = 'Alias'
 		verbose_name_plural = 'Aliases'
+
+        def __unicode__(self):
+                return self.name + '@' + self.domain.name
 
